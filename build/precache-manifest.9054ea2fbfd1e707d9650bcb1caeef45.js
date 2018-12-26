80601334b952916fd111509a6180f2ea8d23f874
ganeshsundaram39 <ganeshsundaram39@gmail.com>
2018-12-26 22:02:36 +0530

    build

---
+++ build/precache-manifest.9054ea2fbfd1e707d9650bcb1caeef45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "536a0578c5f6ddfd034a",
+    "url": "/static/css/main.921dd5ce.chunk.css"
+  },
+  {
+    "revision": "536a0578c5f6ddfd034a",
+    "url": "/static/js/main.536a0578.chunk.js"
+  },
+  {
+    "revision": "b225150249d5fbf43a3b",
+    "url": "/static/js/1.b2251502.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1526a70575080c5aa4834e232a4685db",
+    "url": "/static/media/fruits.1526a705.jpeg"
+  },
+  {
+    "revision": "ee5f0dd9c5a273d751f1c8c2a7942f5b",
+    "url": "/static/media/vegetables.ee5f0dd9.jpg"
+  },
+  {
+    "revision": "bff11cbbb244161dbc10c7b3644068f4",
+    "url": "/index.html"
+  }
+];
