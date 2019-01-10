b39dfdf20bb36b51b3ef0d17516064e8f078f19d
ganeshsundaram39 <ganeshsundaram39@gmail.com>
2019-01-10 21:59:41 +0530

    worked on more-info

--- src/Components/New-Recipe/More-Info/More-Info.js
+++ src/Components/New-Recipe/More-Info/More-Info.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import classes from './More-Info.module.scss';
+import '../../../../node_modules/video-react/dist/video-react.css';
+import { Player } from 'video-react';
+
 export default class MoreInfo extends Component {
   // static propTypes = {};
   state = {
-    video: {
-      src: ''
-    }
+    video: { src: 'https://media.w3.org/2010/05/sintel/trailer_hd.mp4' }
   };
 
   readURL(event) {
     if (event.target.files && event.target.files[0]) {
       var reader = new FileReader();
       reader.onload = e => {
-        console.log(e.target.result);
         this.setState({
           video: {
             src: e.target.result
@@ -32,10 +32,13 @@ export default class MoreInfo extends Component {
           accept="video/*"
           onChange={this.readURL.bind(this)}
         />
-
-        <video width="320" height="240" controls>
-          <source src={this.state.video.src} type="video/mp4" />
-        </video>
+        <Player
+          playsInline
+          src={this.state.video.src}
+          fluid={false}
+          width={'100%'}
+          height={'80%'}
+        />
       </div>
     );
   }
