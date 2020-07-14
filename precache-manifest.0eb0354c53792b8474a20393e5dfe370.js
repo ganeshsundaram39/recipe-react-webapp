ee8d4803147c37cd07ea712831b3096368c308f7
ganeshsundaram39 <ganeshsundaram39@gmail.com>
2020-07-14 21:29:52 +0530

    upload build

---
+++ precache-manifest.0eb0354c53792b8474a20393e5dfe370.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "debfa48f595adfc17e6683ac1865cc7d",
+    "url": "/recipe-react-webapp/index.html"
+  },
+  {
+    "revision": "a103a8080cbe0e08ace9",
+    "url": "/recipe-react-webapp/static/css/main.42369b14.chunk.css"
+  },
+  {
+    "revision": "2fd605e66d9dda0d6ad5",
+    "url": "/recipe-react-webapp/static/js/2.5c261e3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-react-webapp/static/js/2.5c261e3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a103a8080cbe0e08ace9",
+    "url": "/recipe-react-webapp/static/js/main.ee12a005.chunk.js"
+  },
+  {
+    "revision": "77225b68f4fbdbfc0bd0",
+    "url": "/recipe-react-webapp/static/js/runtime-main.87269b61.js"
+  },
+  {
+    "revision": "9d80b34c053592774b6e68cb91fe4579",
+    "url": "/recipe-react-webapp/static/media/chef_dodger.9d80b34c.svg"
+  },
+  {
+    "revision": "98c190b680a016385152ef90818f2fea",
+    "url": "/recipe-react-webapp/static/media/chef_monza.98c190b6.svg"
+  },
+  {
+    "revision": "3f93876e0e95314a7db2aceeb6a625f6",
+    "url": "/recipe-react-webapp/static/media/eating_together_dodger.3f93876e.svg"
+  },
+  {
+    "revision": "750e0fadc210b029388c5024aee71f5f",
+    "url": "/recipe-react-webapp/static/media/eating_together_monza.750e0fad.svg"
+  },
+  {
+    "revision": "3f96560ad92b367405e811562923a92b",
+    "url": "/recipe-react-webapp/static/media/street_food_dodger.3f96560a.svg"
+  },
+  {
+    "revision": "7a604b52702d33f0f080a3981ce099e7",
+    "url": "/recipe-react-webapp/static/media/street_food_monza.7a604b52.svg"
+  },
+  {
+    "revision": "ac395b592f67b4d715f46a05c974f92a",
+    "url": "/recipe-react-webapp/static/media/tasting_dodger.ac395b59.svg"
+  },
+  {
+    "revision": "508e8ad8fc86514313b6fea1e429dff3",
+    "url": "/recipe-react-webapp/static/media/tasting_monza.508e8ad8.svg"
+  }
+]);
